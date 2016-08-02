41757b47f783000a36e71a50885b75bf47785d0c
rockismyth <rockismyth@gmail.com>
2016-08-01 21:07:39 -0400

    Added panels for multiple pages and styled

--- tweettr/config/routes.rb
+++ tweettr/config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'pages/index' => 'pages#index'
-
+  root 'pages#index'
   get 'pages/home' => 'pages#home'
 
   get 'pages/profile' => 'pages#profile'
 
   get 'pages/explore' => 'pages#explore'
 
+  get 'pages/help' => 'pages#help'
+
+  get 'pages/edit' => 'pages#edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
